e5a3eb444f5b263dca80ae0a54fed18cb8030fd0
Natan Gaspar <94939074+NatanGPS@users.noreply.github.com>
2023-02-03 14:00:56 -0300

    Adicionando uma função nova que visa baixar as url

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-
+import os
+import baixar
 
 
 
@@ -13,10 +14,14 @@ class scrapping:
         self.links = self.site.find('div', attrs = {'class': 'module-content'})
         self.urls_para_baixar = []
         self.lista = []
+        self.lista_final = []
+        self.OUTPUT_DIR = "arquivos"
+        self.index = 0
     
     def Iniciar(self):
         self.achar_links()
         self.entrar_links()
+        
 
 # Acha os links  e os adiciona em uma lista 
     def achar_links(self):
@@ -28,14 +33,11 @@ class scrapping:
                 if 'mailto:' not in self.achar_link:
                     self.lista.append(self.achar_link)
 
-        #print(f"Concluido. itens na lista = {len(self.lista)}")
-        print(self.lista)
-
-    # Essa função vai entrar
+    # Essa função vai entrar nos sites e buscar os links de downloads
     def entrar_links(self):
         for entrando in self.lista:
             self.url2 = entrando
-            
+            # faço um tratamento das urls para tornar elas funcionais
             if "http://dados.ufop.br" not in entrando:
                 self.response2 = requests.get('http://dados.ufop.br'+ self.url2)
             
@@ -47,10 +49,25 @@ class scrapping:
             
             for i in self.arquivoscsv.find_all('p'):
                 self.pegar_link = i.get('href')
+                # faço outro tratamento
                 if '/dataset' in self.pegar_link:
                     self.urls_para_baixar.append(self.pegar_link)
         
-        print(self.urls_para_baixar)
+        
+
+
+    def formatar_urls_e_baixar(self):
+        for j in self.urls_para_baixar:
+            if "http://dados.ufop.br" not in j:
+               self.arrumado = self.lista_final.append('http://dados.ufop.br'+ j)
+        
+            else:
+                self.arrumado = self.lista_final.append(j)
+
+            self.nome_arquivo = os.path.join(self.OUTPUT_DIR, f'arquivo{self.index}.csv')
+            baixar.baixar_arquivo(self.arrumado, self.nome_arquivo)
+            self.index += 1
+
 
 
 
