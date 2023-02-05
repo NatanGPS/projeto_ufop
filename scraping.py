2b0cd00abe9697c8a3bfbe47c76c23a433759ffe
Natan Gaspar <94939074+NatanGPS@users.noreply.github.com>
2023-02-05 11:13:08 -0300

    não está funcional ainda

--- scraping.py
+++ scraping.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import os
 
 
 
@@ -13,10 +13,16 @@ class scrapping:
         self.links = self.site.find('div', attrs = {'class': 'module-content'})
         self.urls_para_baixar = []
         self.lista = []
-
+        self.lista_final = []
+        self.OUTPUT_DIR = "output"
+        self.index = 0
+    
     def Iniciar(self):
         self.achar_links()
         self.entrar_links()
+        self.baixar_arquivo(self.filtrar_links(), self.arquivo())
+
+
 # Acha os links  e os adiciona em uma lista 
     def achar_links(self):
         for self.link in self.links.find_all('a', attrs = {'class': 'heading'}):
@@ -28,11 +34,10 @@ class scrapping:
         
        
 
-    # Essa função vai entrar
+    # Essa função vai entrar nos links obtidos pela lista self.lista e vai obter novos links para adicionalos em outros self.urls_para_baixar
     def entrar_links(self):
         for entrando in self.lista:
             self.url2 = entrando
-
             self.response2 = requests.get('http://dados.ufop.br'+ self.url2)
             self.site2 = BeautifulSoup(self.response2.text, 'html.parser')
             self.arquivoscsv = self.site.find('div', attrs = {'class': 'module-content'})
@@ -43,11 +48,30 @@ class scrapping:
                 if 'mailto:' not in self.pegar_link:  
                     self.urls_para_baixar.append(self.pegar_link)
 
-        print(self.urls_para_baixar)
-
-
-
-
+    def arquivo(self):
+        for j in range(len(self.urls_para_baixar)):
+            self.nome_arquivo = os.path.join(self.OUTPUT_DIR, f'arquivo{self.index}.csv')
+            self.index += 1
+    
+    def filtrar_links(self):
+        for self.formata_isso_pfv in self.urls_para_baixar:
+            if "http://dados.ufop.br" not in self.formata_isso_pfv:
+               self.urls = ('http://dados.ufop.br'+ self.formata_isso_pfv)
+    
+            else:
+                self.urls = self.formata_isso_pfv
+# Essa funcao vai baixar os arquivos a partiir das urls da lista self.urls_para_baixar     
+    
+    def baixar_arquivo(self, url, endereco ):
+        
+        #Faz uma requisição ao servidor
+        resposta = requests.get(url)
+        if resposta.status_code == requests.codes.OK:   
+            with open(endereco, 'wb') as arquivo:
+                arquivo.write(resposta.content)
+            print(f"Dowload feito com sucesso. salvo em {endereco}")
+        else:
+            resposta.raise_for_status()
 
 comecar = scrapping()
 comecar.Iniciar()
