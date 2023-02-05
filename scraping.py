6be11117dee979e34d2146db4b848d2a45f6263e
Natan Gaspar <94939074+NatanGPS@users.noreply.github.com>
2023-02-05 14:33:03 -0300

    implementando nova funcao

--- scraping.py
+++ scraping.py
@@ -13,28 +13,43 @@ class scrapping:
         self.links = self.site.find('div', attrs = {'class': 'module-content'})
         self.urls_para_baixar = []
         self.lista = []
-        self.lista_final = []
+        self.lista_inicial = []
         self.OUTPUT_DIR = "output"
         self.index = 0
     
     def Iniciar(self):
+        self.procurando_todos_conjuntos()
         self.achar_links()
         self.entrar_links()
-        #self.arquivo()
         self.filtrar_links()
         
         
+    def procurando_todos_conjuntos(self):
+        url = 'http://dados.ufop.br/group/graduacao'
+        response1 = requests.get(url)
+        site1  = BeautifulSoup(response1.text, 'html.parser')
+        links1 = site1.find('div', attrs = {'class': 'module-content'})
+        
+        for conjunto in links1.find_all('h3', attrs = {'class' : 'dataset-heading'}):
+            buscando_link = conjunto.get('href')
+            if buscando_link != '#':
+                if 'mailto:' not in buscando_link:
+                    if 'http://dados.ufop.br' not in buscando_link:
+                        self.lista_inicial.append('http://dados.ufop.br' + buscando_link)
+                    else:
+                        self.lista_inicial.append(buscando_link)
+
 
 
 # Acha os links  e os adiciona em uma lista 
     def achar_links(self):
-        for self.link in self.links.find_all('a', attrs = {'class': 'heading'}):
-            self.achar_link = self.link.get('href')
-            # esses ifs aninhados servem para filtrar alguns links que nao quero
-            if self.achar_link != '#':
-                if 'mailto:' not in self.achar_link:
-                    self.lista.append(self.achar_link)
-        
+            for self.link in self.links.find_all('a', attrs = {'class': 'heading'}):
+                self.achar_link = self.link.get('href')
+                # esses ifs aninhados servem para filtrar alguns links que nao quero
+                if self.achar_link != '#':
+                    if 'mailto:' not in self.achar_link:
+                        self.lista.append(self.achar_link)
+            
        
 
     # Essa função vai entrar nos links obtidos pela lista self.lista e vai obter novos links para adicionalos em outros self.urls_para_baixar
@@ -52,11 +67,7 @@ class scrapping:
                     if '.csv' in self.pegar_link:
                         self.urls_para_baixar.append(self.pegar_link)
 
-    #def arquivo(self):
-        #for j in range(len(self.urls_para_baixar)):
-            #self.nome_arquivo = os.path.join(self.OUTPUT_DIR, f'arquivo{self.index}.csv')
-            #self.index += 1
-    
+   # Filtra os links e chama a funcao baixar_links
     def filtrar_links(self):
         for self.formata_isso_pfv in self.urls_para_baixar:
             if "http://dados.ufop.br" not in self.formata_isso_pfv:
